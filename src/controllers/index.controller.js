d03ef1c54e47da076304ff21c4ca0c099a304748
garsan84 <jgrsan84@gmail.com>
2021-06-23 14:10:51 +0200

    create function completed

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -11,11 +11,25 @@ const pool = new Pool({
 const getUsers = async (req, res) => {
     const response = await pool.query('SELECT * FROM users');
    
-    res.json(response.rows);
+    res.status(200).json(response.rows);
+}
+
+const createUser = async (req, res) => {
+  const { name, email } = req.body;
+
+  const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)',[name, email]);
+  console.log(response)
+  res.json({
+      message: 'User Added Succesfully',
+      body: {
+          user: {name, email}
+      }
+  })
 }
 
 
 
 module.exports = {
-    getUsers
+    getUsers,
+    createUser
 }
